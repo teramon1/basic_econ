28b53a4f479ff1dbb3689d2deb0441876d52283c
blurry <71152999+teramon1@users.noreply.github.com>
2023-01-08 18:56:59 -0600

    Add files via upload

---
+++ basic_econ/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'blurry'
+description 'read the readme.md'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+server_exports {'GetUserData', 'AddMoney', 'RemoveMoney',}
